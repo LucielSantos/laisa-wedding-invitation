d4956b7d5a436d46ab9872e55620fcda382a693f
LucielSantos <luciel.vitor.bs@gmail.com>
2023-10-31 20:29:22 -0300

    feat: remove head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
-import favicon from "./favicon.ico";
 
 const cormorant = Cormorant_Garamond({
   variable: "--font-primary",
@@ -23,12 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <head>
-        <title>{metadata.title as string}</title>
-        <meta name="description" content={metadata.description as string} />
-        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
-      </head>
-
       <body className={`${cormorant.className} text-brown-500`}>
         {children}
       </body>
