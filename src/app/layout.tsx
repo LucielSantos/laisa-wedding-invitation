2eb4a55d51c3f79e3e5e1d8b6705adaaeeb0c99b
LucielSantos <luciel.vitor.bs@gmail.com>
2023-10-31 20:25:25 -0300

    feat: add favicon src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
+import favicon from "./favicon.ico";
 
 const cormorant = Cormorant_Garamond({
   variable: "--font-primary",
@@ -25,7 +26,7 @@ export default function RootLayout({
       <head>
         <title>{metadata.title as string}</title>
         <meta name="description" content={metadata.description as string} />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
       </head>
 
       <body className={`${cormorant.className} text-brown-500`}>
